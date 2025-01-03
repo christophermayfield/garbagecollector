ba8a0b60d8825e2fc15c67f0434d98646d7d383f
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2025-01-02 18:52:17 -0800

    add header

--- garbage.c
+++ garbage.c
@@ -1,4 +1,9 @@
 #include <stdio.h>   // For standard input/output functions
 #include <stdlib.h>  // For standard library functions
 
+typedef struct header {
+    unsigned int    size;
+    struct header   *next;
+} header_t;
+
 
